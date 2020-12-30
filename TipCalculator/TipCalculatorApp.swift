70e33751c63ea00ebff93022808372a5aba3def5
Mohamed Aymen Afia <mohamedaymen.afia@esprit.tn>
2020-12-30 18:16:34 +0100

    Initial commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Mohamed aymen AFIA on 30/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
